d03b918ca215d04d5e644e5ff1e0af22bd23de20
Gerardo Fernández Moreno <gerardo@latteandcode.com>
2022-06-07 06:52:54 +0000

    7.2: added useEffect in Books component to fetch books

--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -1,3 +1,22 @@
+import {useEffect} from 'react';
+import {useAuthContext} from 'contexts/authContext';
+import apiClient from 'utils/apiClient';
+
 export default function Books() {
+  const {authTokens} = useAuthContext();
+
+  useEffect(
+    function () {
+      async function fetchBooks() {
+        const json = await apiClient.get('/books', {
+          Authorization: `Bearer ${authTokens.token}`
+        });
+        console.log(json);
+      }
+      fetchBooks();
+    },
+    [authTokens.token]
+  );
+
   return <h1>Books component</h1>;
 }
