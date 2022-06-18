485f32f9fef125893be8eed7766e36e328e8ea3a
Gerardo Fernández Moreno <gerardo@latteandcode.com>
2022-06-18 17:32:44 +0000

    18.4: improved BookDetailView

--- src/views/BookDetail/BookDetailView.js
+++ src/views/BookDetail/BookDetailView.js
@@ -1,6 +1,8 @@
 import {generatePath, Link} from 'react-router-dom';
 import {AlertError} from 'components/ui/Alert';
+import Box from 'components/ui/Box';
 import {Button, DangerButton} from 'components/ui/Button';
+import Card from 'components/ui/Card';
 import FlexContainer from 'components/ui/FlexContainer';
 import Loader from 'components/ui/Loader';
 import {BOOK_DELETE, BOOK_EDIT} from 'config/router/paths';
@@ -19,16 +21,42 @@ export default function BookDetailView({book, requestState}) {
   }
 
   return (
-    <div>
-      <h1>{book.title}</h1>
-      <FlexContainer withGutter>
-        <Button as={Link} to={generatePath(BOOK_EDIT, {id: book.id})}>
-          Editar
-        </Button>
-        <DangerButton as={Link} to={generatePath(BOOK_DELETE, {id: book.id})}>
-          Eliminar
-        </DangerButton>
-      </FlexContainer>
-    </div>
+    <Card
+      image={book.image}
+      title={book.title}
+      description={book.description}
+      footer={
+        <div>
+          {book.categories.length > 0 && (
+            <Box marginBottom="1">
+              <ul>
+                {book.categories.map((category) => (
+                  <li key={category.id}>{category.name}</li>
+                ))}
+              </ul>
+            </Box>
+          )}
+          {book.readAt && (
+            <Box marginBottom="1">
+              <strong>Fecha de lectura: </strong>
+              {book.readAt}
+            </Box>
+          )}
+          {book.score && (
+            <Box marginBottom="1">
+              <strong>Puntuación: </strong>: {book.score}
+            </Box>
+          )}
+          <FlexContainer withGutter>
+            <Button as={Link} to={generatePath(BOOK_EDIT, {id: book.id})}>
+              Editar
+            </Button>
+            <DangerButton as={Link} to={generatePath(BOOK_DELETE, {id: book.id})}>
+              Eliminar
+            </DangerButton>
+          </FlexContainer>
+        </div>
+      }
+    ></Card>
   );
 }
